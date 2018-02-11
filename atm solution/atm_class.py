78d38cb10d724b1be0972ff251290aaa57dd927d
Hichem2h <hichem2h@gmail.com>
2018-02-11 21:19:15 +0100

    edit: ATM to give withdraws receipt

--- atm solution/atm_class.py
+++ atm solution/atm_class.py
@@ -1,12 +1,14 @@
 class ATM :
 
     def __init__(self, balance, name):
+        self.withdrawals_list = []
         self.name = name
         self.balance = balance
 
-    def withdraw (self, request):
-        sep = '-' * 23
+    def withdraw(self, request):
+        sep = '=' * 23
 
+        print
         print sep
         print 'Welcome to {}'.format(self.name)
         print 'Current balance = {}'.format(self.balance)
@@ -19,12 +21,27 @@ class ATM :
             print("More than zero plz!")
 
         else:
+            self.withdrawals_list.append(request)
             self.balance -= request
-            cash = [100, 50, 20, 10, 5, 2, 1]
+            cash = [200, 100, 50, 20, 10, 5, 2, 1]
             for e in cash:
                 while request >= e:
                     print 'give {}'.format(e)
                     request -= e
+
+    def show_withdraws(self):
+        sep = '=' * 23
+
+        print
+        print sep
+        print 'Welcome to {}'.format(self.name)
+        print 'Withdraws Receipt'
+        print sep
+
+        for withdrawal in self.withdrawals_list:
+            print 'withdrawal: {}'.format(withdrawal)
+
+
 
 
 if __name__ == '__main__':
@@ -35,7 +52,13 @@ if __name__ == '__main__':
     atm2 = ATM(balance2, "Baraka Bank")
 
     atm1.withdraw(277)
-    atm1.withdraw(800)
+    atm1.withdraw(52)
+    atm1.withdraw(1000)
+    atm1.withdraw(-5)
 
-    atm2.withdraw(100)
-    atm2.withdraw(2000)
+    atm2.withdraw(654)
+    atm2.withdraw(106)
+    atm2.withdraw(900)
+
+    atm1.show_withdraws()
+    atm2.show_withdraws()
